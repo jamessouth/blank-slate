1e6846a8f573449d82b90c240f42d78420fc07a8
jamessouth <jamesdanielsouth@gmail.com>
2020-01-01 16:16:01 -0600

    start UI and websocket

--- src/front/App.js
+++ src/front/App.js
@@ -4,12 +4,32 @@ import React, { useState, useEffect, useReducer } from 'react';
 
 
 export default function App() {
-  const server = 'http://localhost:8000';
+  const server = 'ws://localhost:8000';
+
+  const [hasJoined, setHasJoined] = useState(false);
+  const [answer, setAnswer] = useState('');
 
   
+  useEffect(() => {
+    console.log('connect to server...', Date.now());
 
-  
+    const ws = new WebSocket(server + '/ws');
 
+
+    ws.addEventListener('message', function (e) {
+      console.log(e, Date.now());
+    }, false);
+
+    ws.addEventListener('error', function (e) {
+      console.log(e, Date.now());
+    }, false);
+
+
+    return function cleanup() {
+      ws.close();
+    };
+
+  }, []);
 
 
 
@@ -27,10 +47,19 @@ export default function App() {
       <h1>Blank Slate</h1>
 
 
+      <input value={ answer } onChange={ e => setAnswer(e.target.value) } type="text" placeholder={ hasJoined ? "Answer" : "Name" }/>
+
+      <button
+        type="button"
+        onClick={() => postQuery(queryText)}
+        >
+        submit
+      </button>
 
 
     </main>
   );
 }
 
+// className={ button }
 
