8df3196a59cecc7f7e0a9e32e5c065bf817fd619
jamessouth <jamesdanielsouth@gmail.com>
2020-01-11 15:23:17 -0600

    begin player colors

--- src/server/structs/structs.go
+++ src/server/structs/structs.go
@@ -7,9 +7,16 @@ type Message struct {
 	Time       int    `json:"time,omitempty"`
 }
 
+// Player object with name, score, color
+type Player struct {
+	Name  string `json:"name"`
+	Color string `json:"color"`
+	Score int    `json:"score"`
+}
+
 // PlayersList object with Players property
 type PlayersList struct {
-	Players []string `json:"players"`
+	Players []Player `json:"players"`
 }
 
 // Game object holds game data
@@ -18,6 +25,7 @@ type Game struct {
 	InProgress bool        `json:"inProgress"`
 }
 
+// Word object for game play. Describes a word and whether it comes before or after the blank.
 type Word struct {
 	Word     string `json:"word"`
 	Position string `json:"position"`
