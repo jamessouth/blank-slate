20b6a8ae5ec42beefcb76f09edb6f5e622c86d67
jamessouth <jamesdanielsouth@gmail.com>
2020-01-17 14:28:22 -0600

    animate svg border as timer

--- src/front/components/Word.js
+++ src/front/components/Word.js
@@ -4,7 +4,8 @@ import { div, p } from '../styles/Word.module.css';
 export default function Word({ playerColor, word }) {
 
     return (
-        <div style={{borderColor: playerColor}} className={ div }>
+        <div className={ div }>
+            <svg preserveAspectRatio="none"><rect x="0" y="0" width="100%" style={{stroke: playerColor}} height="100%"/></svg>
             <p className={ p }>{ word }</p>
         </div>
     );
