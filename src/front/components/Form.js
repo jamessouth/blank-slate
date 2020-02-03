4aaa7afd45c67057a927e54efe0f3c44f869c91c
jamessouth <jamesdanielsouth@gmail.com>
2020-02-02 19:28:17 -0600

    add front end input validation

--- src/front/components/Form.js
+++ src/front/components/Form.js
@@ -3,25 +3,31 @@ import { signin, signedin } from '../styles/Form.module.css';
 
 export default function Form({ answered, dupeName, playerName, hasJoined, onEnter, playing }) {
 
+    const regex = /^[a-z0-9 -]+$/i;
+    const NAME_MAX_LENGTH = 10;
+    const ANSWER_MAX_LENGTH = 16;
+    const INPUT_MIN_LENGTH = 2;
     const [inputText, setInputText] = useState('');
-    const [maxLength, setMaxLength] = useState(10);
+    const [maxLength, setMaxLength] = useState(NAME_MAX_LENGTH);
     const [disableSubmit, setDisableSubmit] = useState(true);
+    const [isValidInput, setIsValidInput] = useState(false);
 
+    useEffect(() => {
+      setIsValidInput(regex.test(inputText));
+    }, [inputText]);
 
     useEffect(() => {
       if (hasJoined) {
-        setMaxLength(16);
+        setMaxLength(ANSWER_MAX_LENGTH);
       }
     }, [hasJoined]);
 
     useEffect(() => {
-      setDisableSubmit((inputText.length < 2 || inputText.length > maxLength) || answered || (hasJoined && !playing));
-    }, [inputText, maxLength, answered, hasJoined, playing]);
+      setDisableSubmit((inputText.length < INPUT_MIN_LENGTH || inputText.length > maxLength) || answered || !isValidInput || (hasJoined && !playing));
+    }, [inputText, maxLength, answered, isValidInput, hasJoined, playing]);
 
     return (
-
         <section className={ !hasJoined ? signin : [signin, signedin].join(' ') }>
-     
           <label>{ dupeName ? 'That name is taken!' : playerName ? 'Enter your answer:' : 'Please sign in:'}</label>
           <input
             autoFocus
@@ -37,7 +43,6 @@ export default function Form({ answered, dupeName, playerName, hasJoined, onEnte
             type="text"
             placeholder={ !dupeName && hasJoined ? "Answer" : "Name" }
           />
-
           <button
             type="button"
             onClick={() => {
@@ -49,7 +54,5 @@ export default function Form({ answered, dupeName, playerName, hasJoined, onEnte
             Submit
           </button>
         </section>
-
     );
-
 }
