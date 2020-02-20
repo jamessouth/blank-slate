7d6b5e30169523c6fca96ed07d5f5b379eb8d809
jamessouth <jamesdanielsouth@gmail.com>
2020-02-19 22:40:04 -0600

    move state to custom hook

--- src/front/App.js
+++ src/front/App.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect, useReducer } from 'react';
-import { initialState, reducer } from './reducers/appState';
+import React from 'react';
 import Form from './components/Form';
 import KeepAlive from './components/KeepAlive';
 import Name from './components/Name';
@@ -8,163 +7,36 @@ import Start from './components/Start';
 import Timer from './components/Timer';
 import Word from './components/Word';
 import { div, h1, winner } from './styles/index.css';
-
-const ws = new WebSocket(process.env.WS);
+import useAppState from './utils/useAppState';
 
 export default function App() {
-  const [hasJoined, setHasJoined] = useState(false);
-  const [connected, setConnected] = useState(false);
-  const [showStartTimer, setShowStartTimer] = useState(false);
-  const [answered, setAnswered] = useState(false);
-  const [timer, setTimer] = useState(null);
-  const [submitSignal, setSubmitSignal] = useState(false);
-  const [pingServer, setPingServer] = useState(true);
-  const [showSVGTimer, setShowSVGTimer] = useState(true);
-  const [showReset, setShowReset] = useState(false);
-  const [winners, setWinners] = useState('');
-  const [showWords, setShowWords] = useState(false);
-  const [showStartButton, setShowStartButton] = useState(true);
-  const [gameHasBegun, setGameHasBegun] = useState(false);
-  const [dupeName, setDupeName] = useState(false);
-  const [invalidInput, setInvalidInput] = useState(false);
-  const [playerColor, setPlayerColor] = useState(null);
-  const [
-    {
-      h1Text,
-      newWord,
-      oldWord,
-      playerName,
-      players,
-    },
-    dispatch
-  ] = useReducer(reducer, initialState);
-
-  useEffect(() => {
-    if (invalidInput) {
-      setTimeout(() => {
-        setInvalidInput(false);
-      }, 3750);
-    }
-  }, [invalidInput]);
-
-  useEffect(() => {
-    ws.addEventListener('open', function () {
-      setConnected(true);
-    }, false);
-
-    ws.addEventListener('message', function (e) {
-      const {
-        message,
-        player,
-        players,
-        time,
-        winners,
-        word
-      } = JSON.parse(e.data);
-
-      switch (true) {
-      case !!message:
-        switch (message) {
-        case 'duplicate':
-          setDupeName(true);
-          break;
-        case 'invalid':
-          setInvalidInput(true);
-          break;
-        case 'progress':
-          setGameHasBegun(true);
-          setShowStartTimer(true);
-          break;
-        case 'reset':
-          window.location.reload();
-          break;
-        default: // eslint-disable-next-line no-console
-          console.log('no case for this message found: ', message);
-        }
-        break;
-      case !!player: {
-        const { color, name } = player;
-        setPlayerColor(color);
-        dispatch({ type: 'player', name });
-        setHasJoined(true);
-        break;
-      }
-      case !!players:
-        dispatch({ type: 'players', players });
-        setDupeName(false);
-        break;
-      case !!time:
-        setShowStartTimer(true);
-        setShowStartButton(false);
-        setTimer(time - 1);
-        break;
-      case !!winners:
-        dispatch({ type: 'winners', winners });
-        setWinners(winners)
-        dispatch({ type: 'word', word: '' });
-        setTimeout(() => {
-          setShowReset(true);
-        }, 5000);
-        break
-      case !!word:
-        setPingServer(false);
-        setAnswered(false);
-        setShowSVGTimer(true);
-        dispatch({ type: 'word', word });
-        setShowWords(true);
-        setShowStartTimer(false);
-        break;
-      default: // eslint-disable-next-line no-console
-        console.log('no case found: ', e.data);
-      }
-    }, false);
-
-    ws.addEventListener('error', function (e) { // eslint-disable-next-line no-console
-      console.log(e, Date.now());
-    }, false);
-
-    ws.addEventListener('close', function () {
-      setConnected(false);
-    }, false);
-
-    return function cleanup() {
-      ws.close(1000);
-    };
-  }, []);
-
-  function send(text) {
-    if (!hasJoined) {
-      ws.send(JSON.stringify({
-        name: text,
-      }));
-    } else {
-      setAnswered(true);
-      setSubmitSignal(false);
-      setShowSVGTimer(false);
-      ws.send(JSON.stringify({
-        answer: text,
-      }));
-    }
-  }
-
-  function startGame() {
-    setShowStartButton(false);
-    ws.send(JSON.stringify({
-      message: 'start'
-    }));
-  }
-  
-  function resetGame() {
-    ws.send(JSON.stringify({
-      message: 'reset'
-    }));
-  }
-
-  function pingWS() {
-    ws.send(JSON.stringify({
-      message: 'keepAlive'
-    }));
-  }
+  const {
+    answered,
+    connected,
+    dupeName,
+    gameHasBegun,
+    h1Text,
+    hasJoined,
+    invalidInput,
+    newWord,
+    oldWord,
+    pingServer,
+    pingWS,
+    playerColor,
+    playerName,
+    players,
+    resetGame,
+    send,
+    showReset,
+    showStartButton,
+    showStartTimer,
+    showSVGTimer,
+    showWords,
+    startGame,
+    submitSignal,
+    timer,
+    winners
+  } = useAppState();
 
   return (
     <main>
