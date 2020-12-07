443c20e0815cdf623e080cc43cb4755f9c9ffb23
jamessouth <jamesdanielsouth@gmail.com>
2020-12-06 18:42:16 -0600

    cont auth styling

--- src/front/App.js
+++ src/front/App.js
@@ -11,6 +11,7 @@ import useAppState from './hooks/useAppState';
 
 import Amplify from "aws-amplify";
 import {
+  withAuthenticator,
   AmplifyAuthenticator,
   AmplifyFormSection,
   AmplifySignOut,
@@ -53,76 +54,74 @@ const App = () => {
   } = useAppState();
 
   return (
-    <>
-      <h1 className={[arch, h1].join(' ')}>CLEAN TABLET</h1>
-      <AmplifyAuthenticator>
-        <AmplifyFormSection slot="sign-in" submitButtonText="Sign In">
-          <div slot="amplify-form-section-header">
-            <h3 className={ arch }>SIGN IN</h3>
-          </div>
-          <AmplifyAuthFields formFields={['username', 'password']}/>
-        </AmplifyFormSection>
-        <main>
-          {pingServer && connected &&
-            <KeepAlive
-              pingWS={() => message('keepAlive')} />}
-          <Name
-            playerName={playerName} />
-          <h1 style={{ backgroundColor: playerColor }} className={h1}>{h1Text}</h1>
-          {players.length > 0 && connected &&
-            <Scoreboard
-              playerName={playerName}
+    <main>
+      {pingServer && connected &&
+        <KeepAlive
+          pingWS={() => message('keepAlive')} />}
+      <Name
+        playerName={playerName} />
+      <h1 style={{ backgroundColor: playerColor }} className={h1}>{h1Text}</h1>
+      {players.length > 0 && connected &&
+        <Scoreboard
+          playerName={playerName}
+          players={players}
+          showAnswers={showAnswers}
+          winners={!!winners}
+          word={oldWord} />}
+      {hasJoined && connected &&
+        <div className={div}>
+          {showStartButton && hasJoined &&
+            <Start
+              onClick={() => {
+                message('start');
+                setShowStartButton(false);
+              } }
               players={players}
+              gameHasBegun={gameHasBegun} />}
+          {showStartTimer && timer > 0 &&
+            <Timer
+              timer={timer} />}
+          {showWords && hasJoined && !winners &&
+            <Word
+              onAnimationEnd={() => setSubmitSignal(true)}
+              playerColor={playerColor}
               showAnswers={showAnswers}
-              winners={!!winners}
-              word={oldWord} />}
-          {hasJoined && connected &&
-            <div className={div}>
-              {showStartButton && hasJoined &&
-                <Start
-                  onClick={() => {
-                    message('start');
-                    setShowStartButton(false);
-                  } }
-                  players={players}
-                  gameHasBegun={gameHasBegun} />}
-              {showStartTimer && timer > 0 &&
-                <Timer
-                  timer={timer} />}
-              {showWords && hasJoined && !winners &&
-                <Word
-                  onAnimationEnd={() => setSubmitSignal(true)}
-                  playerColor={playerColor}
-                  showAnswers={showAnswers}
-                  showSVGTimer={showSVGTimer}
-                  word={newWord} />}
-              {winners && <p aria-live="assertive" role="alert" className={winner}>{winners} {winners.includes(' and ') ? 'Win!!' : 'Wins!!'}</p>}
-              {showReset &&
-                <button
-                  type="button"
-                  onClick={() => message('reset')}
-                >
-                      Play Again
-                </button>}
-            </div>}
-          {connected && !winners &&
-            <Form
-              answered={answered}
-              dupeName={dupeName}
-              hasJoined={hasJoined}
-              invalidInput={invalidInput}
-              onEnter={val => send(val)}
-              playerName={playerName}
-              playing={!!newWord}
-              submitSignal={submitSignal} />}
-          {!connected &&
-            <p style={{ fontSize: '18px' }}>Server not available. Please try again.</p>}
-          <footer><a rel="noopener noreferrer" target="_blank" href="https://github.com/jamessouth/clean-tablet">GitHub repo (opens in new tab)</a></footer>
-        </main>
-      </AmplifyAuthenticator>
-    </>
+              showSVGTimer={showSVGTimer}
+              word={newWord} />}
+          {winners && <p aria-live="assertive" role="alert" className={winner}>{winners} {winners.includes(' and ') ? 'Win!!' : 'Wins!!'}</p>}
+          {showReset &&
+            <button
+              type="button"
+              onClick={() => message('reset')}
+            >
+                  Play Again
+            </button>}
+        </div>}
+      {connected && !winners &&
+        <Form
+          answered={answered}
+          dupeName={dupeName}
+          hasJoined={hasJoined}
+          invalidInput={invalidInput}
+          onEnter={val => send(val)}
+          playerName={playerName}
+          playing={!!newWord}
+          submitSignal={submitSignal} />}
+      {!connected &&
+        <p style={{ fontSize: '18px' }}>Server not available. Please try again.</p>}
+      <footer><a rel="noopener noreferrer" target="_blank" href="https://github.com/jamessouth/clean-tablet">GitHub repo (opens in new tab)</a></footer>
+    </main>
   );
 
 }
 
-export default App;
+export default withAuthenticator(App);
+
+/* <AmplifyAuthenticator>
+<AmplifyFormSection slot="sign-in" submitButtonText="Sign In">
+  <div slot="amplify-form-section-header">
+    <h3 className={ arch }>SIGN IN</h3>
+  </div>
+  <AmplifyAuthFields formFields={['username', 'password']}/>
+</AmplifyFormSection> */
+/* </AmplifyAuthenticator> */
