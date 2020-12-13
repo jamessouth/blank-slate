945b355cbb95da1af5a88e1d518096cf32fec710
jamessouth <jamesdanielsouth@gmail.com>
2020-12-13 00:19:15 -0600

    working auth flow

--- src/front/components/Lobby.js
+++ src/front/components/Lobby.js
@@ -1,11 +1,4 @@
 import React from 'react';
-import {
-    withAuthenticator,
-    AmplifyAuthenticator,
-    AmplifySignOut,
-    AmplifyAuthFields,
-    AmplifySignUp,
-  } from '@aws-amplify/ui-react';
 
 
 export default function Lobby() {
