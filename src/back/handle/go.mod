a98240399c7f8b5ce549eb8c79e04fb8fd235e77
jamessouth <jamesdanielsouth@gmail.com>
2020-12-29 00:36:29 -0600

    cont local lambda dev

--- src/back/handle/go.mod
+++ src/back/handle/go.mod
@@ -4,6 +4,7 @@ go 1.15
 
 require (
 	github.com/aws/aws-lambda-go v1.21.0
+	github.com/aws/aws-sdk-go v1.36.15
 	github.com/kr/text v0.2.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 )
--- src/back/handle/main.go
+++ src/back/handle/main.go
@@ -5,6 +5,11 @@ import (
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/request"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
 
 // $env:GOOS = "linux"
@@ -14,12 +19,51 @@ import (
 // build-lambda-zip.exe -o main.zip main
 // sam local invoke HandleFunction -e connect.json
 
+var sess *session.Session
+
+func init() {
+	sess, err := session.NewSession()
+	if err != nil {
+		fmt.Println("session init error")
+	}
+	logger := aws.NewDefaultLogger()
+
+	sess.Handlers.Send.PushFront(func(r *request.Request) {
+		logger.Log(fmt.Sprintf("Request: %s /%v, Payload: %s",
+			r.ClientInfo.ServiceName, r.Operation, r.Params))
+	})
+}
+
 func handler(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 
-	fmt.Println("reqqqq", req)
+	// fmt.Println("reqqqq", req)
+
+	svc := dynamodb.New(sess, aws.NewConfig().WithEndpoint("http://192.168.0.101:8000").WithLogLevel(aws.LogDebugWithHTTPBody))
+
+	// svc.Handlers.Send.PushFront(func(r *request.Request) {
+	// 	r.HTTPRequest.Header.Set("CustomHeader", fmt.Sprintf("%d", 10))
+	// })
+
+	op, err := svc.ListTables(&dynamodb.ListTablesInput{})
+	fmt.Println(op)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case dynamodb.ErrCodeInternalServerError:
+				fmt.Println(dynamodb.ErrCodeInternalServerError, aerr.Error())
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+		}
+		// return
+	}
 
 	return events.APIGatewayProxyResponse{
-		Body:       fmt.Sprintf("Hell549oooo, %v", "hi"),
+		Body:       fmt.Sprintf("tables: , %v", &op.TableNames[0]),
 		StatusCode: 200,
 		Headers: map[string]string{
 			"Content-Type": "application/json",
