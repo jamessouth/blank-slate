dd25aad0a31f512e862ca5a653db389688b29d2a
jamessouth <jamesdanielsouth@gmail.com>
2021-01-14 00:59:00 -0600

    style

--- src/front/index.js
+++ src/front/index.js
@@ -3,8 +3,21 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 import "./styles/index.css";
-// import { arch, div, h1, winner } from './styles/index.css';
 
-ReactDOM.render( React.createElement(React.Fragment, null, React.createElement("h1", {
-  className: "text-6xl mt-11 text-center font-arch decay-mask"
-}, "CLEAN TABLET"), React.createElement(App, null)), document.querySelector("#root"));
+const ce = React.createElement;
+
+ReactDOM.render(
+    ce(
+        React.Fragment,
+        null,
+        ce(
+            "h1",
+            {
+                className: "text-6xl mt-11 text-center font-arch decay-mask",
+            },
+            "CLEAN TABLET"
+        ),
+        ce(App, null)
+    ),
+    document.querySelector("#root")
+);
