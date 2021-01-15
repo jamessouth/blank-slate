cd49f743042c1cf4d95a5e46961a0c40fa230012
jamessouth <jamesdanielsouth@gmail.com>
2021-01-15 01:04:47 -0600

    cont authorizer

--- src/front/components/Lobby.js
+++ src/front/components/Lobby.js
@@ -1,69 +1,93 @@
 import React, { useEffect } from "react";
-import {
-    withAuthenticator,
-    AmplifyAuthenticator,
-    AmplifySignOut,
-    AmplifyAuthFields,
-    AmplifySignUp,
-} from '@aws-amplify/ui-react';
-import { useHistory } from "react-router-dom";
+// import {
+//     withAuthenticator,
+//     AmplifyAuthenticator,
+//     AmplifySignOut,
+//     AmplifyAuthFields,
+//     AmplifySignUp,
+// } from "@aws-amplify/ui-react";
+// import { useHistory } from "react-router-dom";
+
+const ce = React.createElement;
 
 export default function Lobby() {
-    const history = useHistory();
+    // const history = useHistory();
     console.log("lobbbbbbbb");
 
     useEffect(() => {
         const ws = new WebSocket(process.env.CT_WS);
         console.log("pojoihuh", Date.now());
 
-        ws.addEventListener('open', function (e) {
-            console.log(e, Date.now());
-          }, false);
+        ws.addEventListener(
+            "open",
+            function (e) {
+                console.log(e, Date.now());
+            },
+            false
+        );
 
-        ws.addEventListener('error', function (e) {
-            console.log(e, Date.now());
-        }, false);
-        
-        ws.addEventListener('close', function (e) {
-              console.log(e, Date.now());
-              
-            }, false);
-            
-            return function cleanup() {
-              console.log('cleanup');
+        ws.addEventListener(
+            "error",
+            function (e) {
+                console.log(e, Date.now());
+            },
+            false
+        );
+
+        ws.addEventListener(
+            "close",
+            function (e) {
+                console.log(e, Date.now());
+            },
+            false
+        );
+
+        return function cleanup() {
+            console.log("cleanup");
             ws.close(1000);
-          };
+        };
     }, []);
 
-    return (
-        <>
-            {/* <div slot="sign-out">
-                <AmplifySignOut
-                    buttonText="&#128682;"
-                    // handleAuthStateChange={
-                    //     (nextAuthState, authData) => {
-                    //         history.push("/");
-                    //         console.log("authhh", nextAuthState, authData)
-                    //     }
-                    // }
-                />
-            </div> */}
-
-            <div className="flex flex-col mt-8">
-                <button className="mx-auto mb-8 h-40 w-1/2 bg-smoke-100 text-gray-700">
-                    start a new game
-                </button>
-
-                <button className="mx-auto mb-8 h-40 w-1/2 bg-gray-100 text-gray-700">
-                    join
-                </button>
-                <button className="mx-auto mb-8 h-40 w-1/2 bg-gray-100 text-gray-700">
-                    join
-                </button>
-                <button className="mx-auto mb-8 h-40 w-1/2 bg-gray-100 text-gray-700">
-                    join
-                </button>
-            </div>
-        </>
+    return ce(
+        React.Fragment,
+        null,
+        ce(
+            "div",
+            {
+                className: "flex flex-col mt-8",
+            },
+            ce(
+                "button",
+                {
+                    className:
+                        "mx-auto mb-8 h-40 w-1/2 bg-smoke-100 text-gray-700",
+                },
+                "start a new game"
+            ),
+            ce(
+                "button",
+                {
+                    className:
+                        "mx-auto mb-8 h-40 w-1/2 bg-gray-100 text-gray-700",
+                },
+                "join"
+            ),
+            ce(
+                "button",
+                {
+                    className:
+                        "mx-auto mb-8 h-40 w-1/2 bg-gray-100 text-gray-700",
+                },
+                "join"
+            ),
+            ce(
+                "button",
+                {
+                    className:
+                        "mx-auto mb-8 h-40 w-1/2 bg-gray-100 text-gray-700",
+                },
+                "join"
+            )
+        )
     );
 }
--- src/lambda/authorizer/main.go
+++ src/lambda/authorizer/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -11,21 +12,17 @@ import (
 
 func handler(ctx context.Context, req events.APIGatewayCustomAuthorizerRequest) (events.APIGatewayCustomAuthorizerResponse, error) {
 
-	return events.APIGatewayCustomAuthorizerResponse{
-		PrincipalID: "koko",
-		PolicyDocument: events.APIGatewayCustomAuthorizerPolicy{
-			Version: "2012-10-17",
-			Statement: []events.IAMPolicyStatement{{
-				Effect:   "Deny",
-				Action:   []string{"execute-api:Invoke"},
-				Resource: []string{"arn:aws:execute-api:us-east-2:270222239701:m0i5yb937f/dev/$connect"},
-			}},
-		},
-		Context: map[string]interface{}{
-			"ctx": ctx,
-			"req": req,
-		},
-	}, nil
+	fmt.Println("con: ", ctx)
+	fmt.Println("req: ", req)
+
+	val := events.APIGatewayCustomAuthorizerResponse{
+		PrincipalID:        "koko",
+		PolicyDocument:     events.APIGatewayCustomAuthorizerPolicy{Version: "2012-10-17", Statement: []events.IAMPolicyStatement{{Effect: "Allow", Action: []string{"execute-api:Invoke"}, Resource: []string{"arn:aws:execute-api:us-east-2:270222239701:m0i5yb937f/dev/$connect"}}}},
+		Context:            map[string]interface{}{},
+		UsageIdentifierKey: "",
+	}
+
+	return val, nil
 }
 
 func main() {
