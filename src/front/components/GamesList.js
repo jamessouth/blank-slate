077336e89a2430702e0c66a21da7f9ba007ccfa9
jamessouth <jamesdanielsouth@gmail.com>
2021-03-08 22:05:30 -0600

    games list working

--- src/front/components/GamesList.js
+++ src/front/components/GamesList.js
@@ -49,7 +49,14 @@ export default function GamesList({games, send}) {
                     },
                     `${g.no}`
                 ),
-                ce("p", null, g.name)
+                g.players.map(s => ce(
+                    "p",
+                    {
+                        key: s,
+                        // className: "mb-8",
+                    },
+                    s.split("#", 1)[0]
+                ))
             )
         ))
     );
